7c5437691c3f6f05391930d509046b730b47d0b4
清水幸佑 <thimi0412@icloud.com>
2018-05-22 10:16:21 +0900

    del : debug

--- server.py
+++ server.py
@@ -67,8 +67,7 @@ def get_user():
     タスク全取得
     '''
     row = db_connect.get_task()
-    print('xxxxxxxxxxxxxxxxxxxxxxxxx')
-    result = []
+    
     for i in row:
         res_json = {
             'id': int(i[0]),
